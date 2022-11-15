c46f4a800207114fcab0da07590c62599cc9b7f9
Prince <prince.mishra@spit.ac.in>
2022-11-15 21:31:08 +0530

    djdakjs

--- test/product.test.js
+++ test/product.test.js
@@ -1,5 +1,9 @@
 const chai= require('chai');
 const expect=chai.expect;
+const should=chai.should();
+const chaiHttp=require('chai-http')
+const app=require('../app')
+chai.use(chaiHttp)
 
 describe('testing running...',async()=>{
     it('this should compare two numbers',async()=>{
